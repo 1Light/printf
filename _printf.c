7a6e1763a8881220a790b4956b74e02f0761b294
1Light <redclover1.618@gmail.com>
2023-11-14 11:19:06 -0800

    upload

--- _printf.c
+++ _printf.c
@@ -1,156 +1,45 @@
 #include "main.h"
-#include "handlers.h"
-
-int _printf(const char *format, ...);
 /**
-* _printf - prints formatted string to
-* the stdout
-* @format: format specifications or option
-* ...: [ARGUMENTS], varaible params
-*
-* Return: count of characters printed
-* Example:
-*    _printf("%c grade (100%%) in %s", 'A', "0x11");
-*    --> A grade (100%) in 0x11
-*/
-int _printf(const char *format, ...)
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
+ */
+int _printf(const char * const format, ...)
 {
-	short k;
-	bundle b = {{{0}}, {0}, {0}, {0}, {0}, {0}, 0, 0, "", "", NULL, '\0'};
+	convert_match m[] = {
+		{"%s", printf_string}, {"%c", printf_char},
+		{"%%", printf_37},
+		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
+		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+	};
 
-	void (*handlers[])(bundle *) = {handle_character, handle_integer,
-									handle_natural, handle_string};
-	bzero(b.buffer, WRITE_BUFFER_SIZE);
-	va_start(b.list, format);
-	if (format == NULL)
+	va_list args;
+	int i = 0, j, len = 0;
+
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	while (*format)
-	{
-		init_options(&b.opts);
-		b.cfgs.width_malloc = 0;
-		if (*format == '%')
-		{
-			if (*format == 1)
-				return (-1);
-			format++;
-			if (!*format)
-				return (-1);
-			b.cfgs.spec_start = (char *)format;
-			while (*format)
-			{
-				k = b.opts.width + b.opts.precision + b.opts.length;
-				if (strchr(SPECIFIERS, *format) != NULL)
-				{
-					b.opts.conversion = 1;
-					break;
-				}
-				else if (strchr(FLAGS, *format) != NULL)
-				{
-					if (k)
-						break;
-					update_flag(&b.flags, *format);
-					b.opts.flag = 1;
-				}
-				else if ((!k && isdigit(*format)) || (!k && *format == '*'))
-				{
-					if (*format == '*')
-					{
-						format++;
-						b.cfgs.width = va_arg(b.list, int);
-						b.opts.width = 1;
-						continue;
-					}
-					b.tmp = (char *)format++;
-					while (isdigit(*format))
-						format++;
-					bzero(b.conv_buffer, CONV_BUFFER_SIZE);
-					strncpy(b.conv_buffer, b.tmp, format - b.tmp);
-					b.cfgs.width = atoll(b.conv_buffer);
-					b.opts.width = 1;
-					continue;
-				}
-				else if (*format == '.' && !(b.opts.precision + b.opts.length))
-				{
-					b.tmp = (char *) ++format;
-					while (isdigit(*format))
-						format++;
-					if (format - b.tmp > 0)
-					{
-						bzero(b.conv_buffer, CONV_BUFFER_SIZE);
-						strncpy(b.conv_buffer, b.tmp, format - b.tmp);
-						b.cfgs.precision = atoll(b.conv_buffer);
-						b.opts.precision = 1;
-						continue;
-					}
-					break;
-				}
-				else if (strchr(LENGTHS, *format) != NULL && !b.opts.length)
-				{
-					update_length(&b.lengths, *format);
-					b.opts.length = 1;
-				}
-				else
-				{
-					break;
-				}
-				format++;
-			}
 
-			if (!b.opts.conversion)
-			{
-				if (*format)
-				{
-					k = b.opts.conversion + b.opts.length + b.opts.width;
-					k = (k + b.opts.precision) - b.opts.flag;
-					buf_add_ch(b.buffer, &b.cursor, '%', &b.print_counter);
-					if (k == -1)
-						format--;
-					else
-						format = b.cfgs.spec_start;
-				}
-				continue;
-			}
-			b.cur_conv = *format;
-			switch (b.cur_conv)
-			{
-			case 'c':
-				handlers[_character](&b);
-				break;
-			case 's':
-			case 'x':
-			case 'X':
-			case 'p':
-			case 'r':
-			case 'R':
-			case 'S':
-				handlers[_string](&b);
-				break;
-			case '%':
-				buf_add_ch(b.buffer, &b.cursor, b.cur_conv, &b.print_counter);
-				break;
-			case 'd':
-			case 'i':
-				handlers[_integer](&b);
-			break;
-			case 'o':
-			case 'u':
-			case 'b':
-				handlers[_natural](&b);
-				break;
-			default:
-				break;
-			}
-		}
-		else
-		{
-			buf_add_ch(b.buffer, &b.cursor, *format, &b.print_counter);
-		}
-		format++;
-	}
-	if (b.cursor > 0)
+Here:
+	while (format[i] != '\0')
 	{
-		_write(b.buffer, b.cursor);
+		j = 13;
+		while (j >= 0)
+		{
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		len++;
+		i++;
 	}
-	va_end(b.list);
-	return (b.print_counter);
+	va_end(args);
+	return (len);
 }
--- main.h
+++ main.h
@@ -1,144 +1,49 @@
-#ifndef GroupPrintf
-#define GroupPrintf
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
 #include <stdio.h>
-#include <ctype.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
 #include <limits.h>
-#include <stdbool.h>
+#include <unistd.h>
 
-#define FLAGS  "+- 0#"
-#define LENGTHS "lh"
-#define OPTIONS "FWPL"
-#define SPECIFIERS "dixXsSpoucbrR%"
-#define WRITE_BUFFER_SIZE 1024
-#define CONV_BUFFER_SIZE 40
-#define BASE_10 10
-#define BASE_2 2
-#define BASE_8 8
+
 
 /**
-* struct opt_flag - structure holding flags
-* @plus: + flag
-* @minus: - flag
-* @space: space flag
-* @hash: # flag
-* @zero: 0 flag
-*/
-typedef struct opt_flag
-{
-	short plus;
-	short minus;
-	short space;
-	short hash;
-	short zero;
-} opt_flag;
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
+ *
+ */
 
-/**
-* struct option - structure holding options
-* @flag: flags
-* @width: width
-* @precision: precision
-* @length: lendth
-* @conversion: conversion
-*/
-typedef struct option
+typedef struct format
 {
-	short flag;
-	short width;
-	short precision;
-	short length;
-	short conversion;
-} option;
+	char *id;
+	int (*f)();
+} convert_match;
 
-/**
-* struct opt_length - structure holding lengths
-* @_long: long integer
-* @_short: shot integer
-*/
-typedef struct opt_length
-{
-	short _long;
-	short _short;
-} opt_length;
-
-/**
-* struct config - structure holding configs
-* @width: value of width
-* @precision: value of precision
-* @spec_start: position after %
-* @width_buffer: buffer if width is set
-* @width_malloc: flag if there is malloc for width
-* @tmp_malloc: flag if there is malloc for tmp
-*/
-typedef struct config
-{
-	uint64_t width;
-	uint64_t precision;
-	char *spec_start;
-	char *width_buffer;
-	short width_malloc;
-	short tmp_malloc;
-} config;
-
-/**
-* struct param - structure holding variadic params
-* @Int: for signed integers
-* @UInt: for unsigned integers
-* @String: for strings
-*/
-typedef struct param
-{
-	int64_t Int;
-	uint64_t UInt;
-	char *String;
-} param;
-
-/**
- * struct bundle - structure hundler bundle of data objects
- * @list: variable list
- * @flags: flags
- * @lengths: lengths
- * @cfgs: configs
- * @opts: options
- * @params: paramters
- * @cursor: buffer cursor
- * @print_counter: for characters printed
- * @buffer: for writing
- * @conv_buffer: for conversions
- * @tmp: temporal helper
- * @cur_conv: current format character
-*/
-typedef struct bundle
-{
-	va_list list;
-	opt_flag flags;
-	opt_length lengths;
-	config cfgs;
-	option opts;
-	param params;
-	short cursor;
-	uint64_t print_counter;
-	char buffer[WRITE_BUFFER_SIZE];
-	char conv_buffer[CONV_BUFFER_SIZE];
-	char *tmp;
-	char cur_conv;
-} bundle;
-
-int _write(char *buffer, int bytes);
+int printf_pointer(va_list val);
+int printf_hex_aux(unsigned long int num);
+int printf_HEX_aux(unsigned int num);
+int printf_exclusive_string(va_list val);
+int printf_HEX(va_list val);
+int printf_hex(va_list val);
+int printf_oct(va_list val);
+int printf_unsigned(va_list args);
+int printf_bin(va_list val);
+int printf_srev(va_list args);
+int printf_rot13(va_list args);
+int printf_int(va_list args);
+int printf_dec(va_list args);
+int _strlen(char *s);
+int *_strcpy(char *dest, char *src);
+int _strlenc(const char *s);
+int rev_string(char *s);
+int _strlenc(const char *s);
+int printf_37(void);
+int printf_char(va_list val);
+int printf_string(va_list val);
+int _putchar(char c);
 int _printf(const char *format, ...);
-char *base_conv(int64_t n, short base, char *buffer);
-char *dec2hex(uint64_t n, char _case, char *buffer);
-void update_flag(opt_flag *flags, char flag);
-void update_length(opt_length *lengths, char length);
-void init_options(option *options);
-void buf_add_ch(char *buffer, short *cursor, char ch, uint64_t *pc);
-void buf_add_str(char *buffer, short *cursor, char *str, uint64_t *pc);
-void _strrev(char *buffer, short *cursor, char *s, uint64_t *pc);
-void _rot13(char *buffer, short *cursor, char *s, uint64_t *pc);
-void w_buf_add_str(config *cfgs, opt_flag *flgs, char **data);
 
 #endif
